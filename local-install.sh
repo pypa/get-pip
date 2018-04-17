448c64c4f50e26dfe162b8b5c016f1ceb3c82385
Hugo <hugovk@users.noreply.github.com>
2018-04-17 13:07:30 +0300

    Test local install

---
+++ local-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Exit on error, run verbose
+set -ev
+
+# Run from a script to make sure Travis CI handles failures properly:
+# http://steven.casagrande.io/articles/travis-ci-and-if-statements/
+
+if   [ $TRAVIS_PYTHON_VERSION == 2.6 ]; then python 2.6/get-pip.py;
+elif [ $TRAVIS_PYTHON_VERSION == 3.2 ]; then python 3.2/get-pip.py;
+elif [ $TRAVIS_PYTHON_VERSION == 3.3 ]; then python 3.3/get-pip.py;
+else                                         python get-pip.py;
+fi
+
+exit 0;
