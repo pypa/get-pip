6509181faced47c0680f5c2521933e044bdc6f1f
Hugo <hugovk@users.noreply.github.com>
2018-04-18 15:35:29 +0300

    Remove redundant trailing semicolons

--- curl-install.sh
+++ curl-install.sh
@@ -6,10 +6,10 @@ set -euvo pipefail
 # Run from a script to make sure Travis CI handles failures properly:
 # http://steven.casagrande.io/articles/travis-ci-and-if-statements/
 
-if   [ $TRAVIS_PYTHON_VERSION == 2.6 ]; then curl https://bootstrap.pypa.io/2.6/get-pip.py | python;
-elif [ $TRAVIS_PYTHON_VERSION == 3.2 ]; then curl https://bootstrap.pypa.io/3.2/get-pip.py | python;
-elif [ $TRAVIS_PYTHON_VERSION == 3.3 ]; then curl https://bootstrap.pypa.io/3.3/get-pip.py | python;
-else                                         curl https://bootstrap.pypa.io/get-pip.py | python;
+if   [ $TRAVIS_PYTHON_VERSION == 2.6 ]; then curl https://bootstrap.pypa.io/2.6/get-pip.py | python
+elif [ $TRAVIS_PYTHON_VERSION == 3.2 ]; then curl https://bootstrap.pypa.io/3.2/get-pip.py | python
+elif [ $TRAVIS_PYTHON_VERSION == 3.3 ]; then curl https://bootstrap.pypa.io/3.3/get-pip.py | python
+else                                         curl https://bootstrap.pypa.io/get-pip.py | python
 fi
 
-exit 0;
+exit 0
--- local-install.sh
+++ local-install.sh
@@ -6,10 +6,10 @@ set -euvo pipefail
 # Run from a script to make sure Travis CI handles failures properly:
 # http://steven.casagrande.io/articles/travis-ci-and-if-statements/
 
-if   [ $TRAVIS_PYTHON_VERSION == 2.6 ]; then python 2.6/get-pip.py;
-elif [ $TRAVIS_PYTHON_VERSION == 3.2 ]; then python 3.2/get-pip.py;
-elif [ $TRAVIS_PYTHON_VERSION == 3.3 ]; then python 3.3/get-pip.py;
-else                                         python get-pip.py;
+if   [ $TRAVIS_PYTHON_VERSION == 2.6 ]; then python 2.6/get-pip.py
+elif [ $TRAVIS_PYTHON_VERSION == 3.2 ]; then python 3.2/get-pip.py
+elif [ $TRAVIS_PYTHON_VERSION == 3.3 ]; then python 3.3/get-pip.py
+else                                         python get-pip.py
 fi
 
-exit 0;
+exit 0
