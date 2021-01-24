bb9b4f45a01c210f51230cda8827f21fa4cab29c
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-01-24 14:58:07 +0000

    Add "scripts/generate.py" and setup nox session
    This reintroduces the logic we had for generating our get-pip.py scripts
    with 3 major changes:
    
    - drop dependency on invoke.
    - add caching using requests and cachecontrol.
    - setup a nox session to orchestrate everything.

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,12 @@
 import os
+from pathlib import Path
 
 import nox
+
+
+@nox.session
+def generate(session):
+    """Update the scripts, to the latest versions."""
+    session.install("packaging", "requests", "cachecontrol[filecache]")
+
+    session.run("python", "scripts/generate.py")
