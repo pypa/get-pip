35c7246f63515800d2c54bc0150c31e0525484d9
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-02-06 15:42:46 +0000

    Drop base64.b85decode fallback
    This was added in Python 3.4, which is now below the oldest supported
    Python version.

--- templates/default.py
+++ templates/default.py
@@ -24,57 +24,8 @@ import os.path
 import pkgutil
 import shutil
 import sys
-import struct
 import tempfile
-
-# Useful for very coarse version differentiation.
-PY2 = sys.version_info[0] == 2
-PY3 = sys.version_info[0] == 3
-
-if PY3:
-    iterbytes = iter
-else:
-    def iterbytes(buf):
-        return (ord(byte) for byte in buf)
-
-try:
-    from base64 import b85decode
-except ImportError:
-    _b85alphabet = (b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                    b"abcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{{|}}~")
-
-    def b85decode(b):
-        _b85dec = [None] * 256
-        for i, c in enumerate(iterbytes(_b85alphabet)):
-            _b85dec[c] = i
-
-        padding = (-len(b)) % 5
-        b = b + b'~' * padding
-        out = []
-        packI = struct.Struct('!I').pack
-        for i in range(0, len(b), 5):
-            chunk = b[i:i + 5]
-            acc = 0
-            try:
-                for c in iterbytes(chunk):
-                    acc = acc * 85 + _b85dec[c]
-            except TypeError:
-                for j, c in enumerate(iterbytes(chunk)):
-                    if _b85dec[c] is None:
-                        raise ValueError(
-                            'bad base85 character at position %d' % (i + j)
-                        )
-                raise
-            try:
-                out.append(packI(acc))
-            except struct.error:
-                raise ValueError('base85 overflow in hunk starting at byte %d'
-                                 % i)
-
-        result = b''.join(out)
-        if padding:
-            result = result[:-padding]
-        return result
+from base64 import b85decode
 
 
 def bootstrap(tmpdir=None):
