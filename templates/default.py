29f37dbe6b3842ccd52d61816a3044173962ebeb
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-03-14 19:09:33 +0000

    Fix links to legacy Python versions

--- templates/default.py
+++ templates/default.py
@@ -28,7 +28,7 @@ if this_python < min_version:
     message_parts = [
         "This script does not work on Python {{}}.{{}}".format(*this_python),
         "The minimum supported Python version is {{}}.{{}}.".format(*min_version),
-        "Please use https://bootstrap.pypa.io/{{}}.{{}}/get-pip.py instead.".format(*this_python),
+        "Please use https://bootstrap.pypa.io/pip/{{}}.{{}}/get-pip.py instead.".format(*this_python),
     ]
     print("ERROR: " + " ".join(message_parts))
     sys.exit(1)
