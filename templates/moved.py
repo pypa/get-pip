8a2715bff4ab1e7a5237905c885ea7d7e002a6e3
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-05-22 12:46:36 +0100

    Revert "Update moved.py template for Mar 2021 renaming"
    This reverts commit b44c5248c6916cbfa650e419fc8fb9ed39dd6a44.

--- templates/moved.py
+++ templates/moved.py
@@ -12,19 +12,6 @@ longer work. Please use get-pip.py from the following URL instead:
     https://bootstrap.pypa.io/{location}
 
 Sorry if this change causes any inconvenience for you!
-
-We don't have a good mechanism to make more gradual changes here, and this
-renaming is a part of an effort to make it easier to us to update these
-scripts, when there's a pip release. It's also essential for improving how we
-handle the `get-pip.py` scripts, when pip drops support for a Python minor
-version.
-
-There are no more renames/URL changes planned, and we don't expect that a need
-would arise to do this again in the near future.
-
-Thanks for understanding!
-
-- Pradyun, on behalf of the volunteers who maintain pip.
 """
 
 print(message, file=sys.stderr)
